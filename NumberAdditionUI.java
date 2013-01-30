01167e3aca94b37decba1a20c8fffacf317aa4ab
Tyler Childs <datetyler@gmail.com>
2013-01-30 12:16:58 -0500

    Library Linked!

--- NumberAdditionUI.java
+++ NumberAdditionUI.java
@@ -2,7 +2,8 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package my.numberaddition;
+//package my.numberaddition;
+import it.sauronsoftware.ftp4j.*;
 
 /**
  *
@@ -216,7 +217,7 @@ public class NumberAdditionUI extends javax.swing.JFrame {
         int port;
         
         //Initiate FTP Class
-        //FTPClient client = new FTPClient();
+        FTPClient client = new FTPClient();
         
         
         
